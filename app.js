af6f7ab4866827e204167044944d7640ab521739
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-03-28 14:07:31 -0300

    Agregando nuevos cambios de prueba

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+console.log(indexRouter);
 var app = express();
 
 // view engine setup
